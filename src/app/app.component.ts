6e21d8c126c9ea10840e742c37b15d926252ca22
Edward Apostol <edward.apostol.github@gmail.com>
2016-11-09 07:10:12 -0500

    #004: added basic TOH files
    added basic TOH files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+// templateUrl: './app.component.html',
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-app',
+    templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes!';
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
